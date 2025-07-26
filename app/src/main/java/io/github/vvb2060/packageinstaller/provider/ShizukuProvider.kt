a27483cdea352a53f976186a3d3969352aa9e519
vvb2060 <vvb2060@gmail.com>
2025-07-27 05:12:07 +0800

    auto addPreferredActivity

---
+++ app/src/main/java/io/github/vvb2060/packageinstaller/provider/ShizukuProvider.kt
@@ -0,0 +1,18 @@
+package io.github.vvb2060.packageinstaller.provider
+
+import android.content.pm.PackageManager
+import io.github.vvb2060.packageinstaller.model.Hook
+import rikka.shizuku.Shizuku
+
+class ShizukuProvider : rikka.shizuku.ShizukuProvider() {
+
+    override fun onCreate(): Boolean {
+        Shizuku.addBinderReceivedListener {
+            if (Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED) {
+                Hook.init(context!!)
+                Hook.addPreferredActivity(context!!.packageManager)
+            }
+        }
+        return super.onCreate()
+    }
+}
